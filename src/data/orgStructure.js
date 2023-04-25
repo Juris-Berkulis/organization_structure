eeb292ab3f1ce7d90de143e20fba5097b69b1b39
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-04-25 18:45:05 +0300

    1. Change the organization structure in the files "src/data/orgStructure.js" and "src/data/orgStructure.json".

--- src/data/orgStructure.js
+++ src/data/orgStructure.js
@@ -24,9 +24,6 @@ const orgStructure = [
                     {
                         name: "Отдел 6",
                     },
-                    {
-                        name: "Отдел 444",
-                    },
                 ],
             },
             {
