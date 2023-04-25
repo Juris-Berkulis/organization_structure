62f6afe82e756a12ddac5a72324103091c3d75d8
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-04-25 18:19:06 +0300

    1. Create scripts in the "package.json" file for deploying the project to hostings and for copying the organization structure from the "src/data/orgStructure.js" file to the "src/data/orgStructure.json" file; 2. Change the development mode in the "vite.config.js" file; 3. Add automatically to the ".gitignore" file of a ignoringing the ".netlify" and ".vercel" folders.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  mode: 'development',
+  mode: 'production',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
